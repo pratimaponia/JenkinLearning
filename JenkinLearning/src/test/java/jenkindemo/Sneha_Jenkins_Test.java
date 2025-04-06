01c678369856689c315820c1e9a46f23cbdd359e
HP <testingamazon646@gmail.com>
2025-04-06 13:07:27 +0530

    sneha commet to pratimas project

---
+++ JenkinLearning/src/test/java/jenkindemo/Sneha_Jenkins_Test.java
@@ -0,0 +1,13 @@
+package jenkindemo;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Sneha_Jenkins_Test {
+	@Test
+	public void Bark() {
+		Reporter.log("sneha code pass",true);
+	}
+	
+
+}
