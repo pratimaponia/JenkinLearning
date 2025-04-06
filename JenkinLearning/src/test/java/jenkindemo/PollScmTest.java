427f51001df0a9aa684989f869eb5bee9bb9b32e
prath <prath@Pratha>
2025-04-06 12:12:47 +0530

    Poll scm method added

---
+++ JenkinLearning/src/test/java/jenkindemo/PollScmTest.java
@@ -0,0 +1,14 @@
+package jenkindemo;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class PollScmTest {
+	
+	@Test
+	public void test()
+	{
+		Reporter.log("Poll SCRM class added", true);
+	}
+
+}
